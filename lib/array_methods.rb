2077fc56338353a81908c69d0b7808ffb193f482
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-11 04:58:15 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
@@ -19,5 +19,5 @@ def using_last(array)
 end
 
 def using_size(array)
-
+  array.size
 end
