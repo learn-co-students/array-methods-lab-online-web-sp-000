41fc2671efcb2f56b9bfdb8adb1bd09787bf6c1e
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 20:10:40 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
+  array.reverse
 end
 
 def using_first(array)
-
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
+  array.first
 end
 
 def using_last(array)
-
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+  ["wow","blue", "yellow", "orange", "green","arrays!"]
+  array.size
 end
