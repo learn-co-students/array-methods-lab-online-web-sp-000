f960b4d801f3c7a895ebe3d75b716fc7aa583fb9
Daysha Jones <daysha.vu@icloud.com>
2021-03-12 19:11:21 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+dogs = ["toby", "I", "wow"]
+dogs.sort
 end
 
 def using_reverse(array)
-
+cats = ["wow", "Flufy", "Lady", "arrays!"]
+cats.reverse
 end
 
 def using_first(array)
-
+exciting = ["wow", "cool", "crazy"]
+exciting.first
 end
 
 def using_last(array)
-
+computer = ["notes", "study", "arrays!"]
+computer.last
 end
 
 def using_size(array)
-
+toystory = ["Buzz", "Woody", "Jessie", "Forky", "Rex", "Potato Head"]
+toystory.size
 end
