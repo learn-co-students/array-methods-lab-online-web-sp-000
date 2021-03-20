27e8f5c2b18793433ace50dac4cd8f514b76a710
Craig <suesscraig112715@gmail.com>
2021-03-20 16:22:49 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+   array.include?(element);             #does the array contain this element. 
 end
 
 def using_sort(array)
-
+  array.sort();                         #sort array alphabetically or from least to greatest.
 end
 
 def using_reverse(array)
-
+  array.reverse();                      #reverse contents inside of array. 
 end
 
 def using_first(array)
-
+  array.first();                        #return the first element of the array. 
 end
 
 def using_last(array)
-
+  array.last();                         #return the last element of the array. 
 end
 
 def using_size(array)
-
+  array.size();                         #return the size of the array, counting from 1. 
 end
